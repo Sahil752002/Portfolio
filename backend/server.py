f65d9c4cf63c456e7023f2360674ed3c54d81a79
Sahil752002 <78405404+Sahil752002@users.noreply.github.com>
2025-08-11 19:49:32 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
@@ -10,7 +11,6 @@ from pydantic import BaseModel, Field, EmailStr, ValidationError
 from typing import List, Optional
 import uuid
 from datetime import datetime
-from contextlib import asynccontextmanager
 
 
 ROOT_DIR = Path(__file__).parent
